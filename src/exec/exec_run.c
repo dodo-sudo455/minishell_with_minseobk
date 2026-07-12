1fa7827f4906206e762c28968b3b1250378ca8e0
msisdev <33077839+msisdev@users.noreply.github.com>
2026-07-12 20:32:24 +0900

    backup

---
+++ src/exec/exec_run.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_run.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minseobk <minseobk@student.42gyeongsan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/12 20:22:10 by minseobk          #+#    #+#             */
+/*   Updated: 2026/07/12 20:26:46 by minseobk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+
+/**
+ *	DESCRIPTION
+ *
+ *		For each cmd:
+ *			- pipe
+ *			- open redir
+ *			- exec
+ */
+t_error	exec_run(t_ctx*c_ref, t_lst *cmdlst_ref)
+{
+	(void)c_ref;
+	(void)cmdlst_ref;
+	return (ERROR_DEBUG);
+}
